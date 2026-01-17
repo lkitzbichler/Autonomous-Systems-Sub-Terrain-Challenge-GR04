8b9ea8c79e4b48d8638a4ee1cda99f69f12941d0
Leon Kitzbichler <lkitzbichler@gmail.com>
2026-01-17 17:32:36 +0100

    Ignore ROS2 build/install/log

--- ros2_ws/build/mav_msgs/CMakeFiles/mav_msgs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mav_msgs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mav_msgs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
