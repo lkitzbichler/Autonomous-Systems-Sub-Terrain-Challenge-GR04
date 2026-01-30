e40b4bc46d389b7c1c86f6dd71985ed7f8fbe35d
Leon Kitzbichler <lkitzbichler@gmail.com>
2026-01-30 15:32:36 +0100

    made install simulation automatically

--- build.bash
+++ build.bash
@@ -14,36 +14,36 @@ cd "${ROS_WS}"
 colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 echo "Build OK."
 
-echo "[2/4] Ensuring destination exists..."
-mkdir -p "${SIM_DST}"
-
-echo "[3/4] Copy simulation runtime files if missing..."
-shopt -s nullglob dotglob
-for item in "${SIM_SRC}"/*; do
-  base="$(basename "${item}")"
-  src="${item}"
-  dst="${SIM_DST}/${base}"
-
-  if [[ -e "${dst}" ]]; then
-    echo "  - Skipping (exists): ${base}"
-  else
-    echo "  - Copying: ${base}"
-    cp -a "${src}" "${dst}"
-  fi
-done
-shopt -u nullglob dotglob
-
-echo "[4/4] Ensuring executable bit on Simulation.x86_64..."
-BIN="${SIM_DST}/Simulation.x86_64"
-if [[ -f "${BIN}" ]]; then
-  if [[ -x "${BIN}" ]]; then
-    echo "  - Already executable: Simulation.x86_64"
-  else
-    echo "  - Setting executable: Simulation.x86_64"
-    sudo chmod +x "${BIN}"
-  fi
-else
-  echo "WARN: ${BIN} not found (did copy succeed?)"
-fi
+# echo "[2/4] Ensuring destination exists..."
+# mkdir -p "${SIM_DST}"
+#
+# echo "[3/4] Copy simulation runtime files if missing..."
+# shopt -s nullglob dotglob
+# for item in "${SIM_SRC}"/*; do
+#   base="$(basename "${item}")"
+#   src="${item}"
+#   dst="${SIM_DST}/${base}"
+#
+#   if [[ -e "${dst}" ]]; then
+#     echo "  - Skipping (exists): ${base}"
+#   else
+#     echo "  - Copying: ${base}"
+#     cp -a "${src}" "${dst}"
+#   fi
+# done
+# shopt -u nullglob dotglob
+#
+# echo "[4/4] Ensuring executable bit on Simulation.x86_64..."
+# BIN="${SIM_DST}/Simulation.x86_64"
+# if [[ -f "${BIN}" ]]; then
+#   if [[ -x "${BIN}" ]]; then
+#     echo "  - Already executable: Simulation.x86_64"
+#   else
+#     echo "  - Setting executable: Simulation.x86_64"
+#     sudo chmod +x "${BIN}"
+#   fi
+# else
+#   echo "WARN: ${BIN} not found (did copy succeed?)"
+# fi
 
 echo "Done."
